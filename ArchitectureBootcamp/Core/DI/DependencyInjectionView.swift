b5c1290d3bec9dddf3093eaf14f9ec10169436b1
jason <15021453094@163.com>
2026-05-28 16:15:07 +0800

    [Feature] DependencyContainer

---
+++ ArchitectureBootcamp/Core/DI/DependencyInjectionView.swift
@@ -0,0 +1,91 @@
+//
+//  DependencyInjectionView.swift
+//  ArchitectureBootcamp
+//
+//  Created by Elaine on 2026/5/28.
+//
+
+import SwiftUI
+
+// Dependency Injection
+// https://github.com/Swinject/Swinject
+
+@MainActor
+@Observable
+class UserManager {
+    
+}
+
+@MainActor
+@Observable
+class DependencyInjectionViewModel {
+    
+    private let dataManager: DataManager
+    private let userManager: UserManager
+    
+    // 注入 DependencyContainer
+    init(container: DependencyContainer) {
+        self.dataManager = container.resolve(DataManager.self)!
+        self.userManager = container.resolve(UserManager.self)!
+    }
+    
+    // ViewModel holds the array of products
+    var products: [Product] = []
+    
+    func loadData() async {
+        do {
+            products = try await dataManager.getProducts()
+        } catch {
+            print("error loading data: \(error.localizedDescription)")
+        }
+    }
+}
+
+struct DependencyInjectionView: View {
+    @State var vm: DependencyInjectionViewModel
+    var body: some View {
+        VStack {
+            ForEach(vm.products) { product in
+                Text(product.title)
+            }
+        }
+        .task {
+            await vm.loadData()
+        }
+    }
+}
+
+/// 存储所有的 manager
+class DependencyContainer {
+    private var managers: [String: Any] = [:]
+    
+    // 注册 manager
+    func regiser<T>(_ type: T.Type, manager: T) {
+        let key = "\(type)"
+        managers[key] = manager
+    }
+    
+    func register<T>(_ type: T.Type, manager: () -> T) {
+        let key = "\(type)"
+        managers[key] = manager()
+    }
+    
+    // 获取 manager
+    func resolve<T>(_ type: T.Type) -> T? {
+        let key = "\(type)"
+        return managers[key] as? T
+    }
+}
+
+#Preview {
+    let container = DependencyContainer()
+    container.regiser(DataManager.self, manager: DataManager(service: MockDataService()))
+    container.regiser(UserManager.self, manager: UserManager())
+    container.register(UserManager.self) {
+        UserManager()
+    }
+    
+    return DependencyInjectionView(
+        vm: DependencyInjectionViewModel(container: container)
+    )
+}
