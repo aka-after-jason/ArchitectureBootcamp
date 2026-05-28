e499916dab43f56dea2363a30576c64ce023af04
jason <15021453094@163.com>
2026-05-28 10:23:47 +0800

    Initial Commit

---
+++ ArchitectureBootcamp/ArchitectureBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchitectureBootcampApp.swift
+//  ArchitectureBootcamp
+//
+//  Created by Elaine on 2026/5/28.
+//
+
+import SwiftUI
+
+@main
+struct ArchitectureBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
